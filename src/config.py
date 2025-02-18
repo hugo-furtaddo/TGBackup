477331a1d4c43b79a487eb2453a6101f9973aa90
Your Name <you@example.com>
2025-02-18 14:05:05 -0300

    Adiciona módulo de configuração

---
+++ src/config.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import json
+
+CONFIG = json.load(
+    open(os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), "config.json"))
+)
+
+
+def get_verbose():
+    return CONFIG["verbose"]
+
+
+def get_telegram_api_id():
+    return CONFIG["telegram_api_id"]
+
+
+def get_telegram_api_hash():
+    return CONFIG["telegram_api_hash"]
+
+def get_phone():
+    return CONFIG["phone_number"]
+
+def get_db_file():
+    return CONFIG["db_file"]
